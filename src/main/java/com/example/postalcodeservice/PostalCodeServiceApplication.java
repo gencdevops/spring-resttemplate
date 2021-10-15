f4284ba6cae226ca51ba2aa10bc1c8f274556aea
gencdevops <gencdevops@gmail.com>
2021-10-15 08:54:45 +0300

    Initial commit

---
+++ src/main/java/com/example/postalcodeservice/PostalCodeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.postalcodeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostalCodeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostalCodeServiceApplication.class, args);
+    }
+
+}
