49fada4d67fb8c5a36d47435303703f399d36bfd
gencdevops <gencdevops@gmail.com>
2021-10-15 09:10:01 +0300

    PostalCode PostalCodeInfo

---
+++ src/main/java/com/example/postalcodeservice/geonamesdto/PostalCodeInfo.java
@@ -0,0 +1,12 @@
+package com.example.postalcodeservice.geonamesdto;
+
+import java.util.List;
+
+public class PostalCodeInfo {
+    private List<PostalCode> postalCodes;
+
+    public List<PostalCode> getPostalCodes() {
+        return postalCodes;
+    }
+
+}
